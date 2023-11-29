f60b1134db81d35fcfc36cc44095301cceb84f56
Vilma Kankaanpää <vilma.kankaanpaa@valonaintelligence.com>
2023-11-29 11:32:04 +0200

    edits to unit test with pylint

---
+++ mathematics_test.py
@@ -0,0 +1,14 @@
+import mathematics
+
+class TestMathMethods():
+
+    def test_add(self):
+        assert 3 == mathematics.add(1, 2)
+        assert 10 == mathematics.add(3, 7)
+
+    def test_multiply(self):
+        assert 50 == mathematics.multiply(5, 10)
+
+    def test_power(self):
+        assert 256 == mathematics.power(2, 8)
+                         
